45242be340ffcb27f56538c76403d2f0dc799cb8
Ken Schwencke <schwank@gmail.com>
2013-11-06 09:47:23 -0800

    Added a monkey patch to all models globally, and now we are binding a popup with all of the DB columns that aren't geometries

--- kowloon/views.py
+++ kowloon/views.py
@@ -1,8 +1,21 @@
+import json
 from django.db.models.base import ModelBase
 from kowloon import models as database_models
 from django.shortcuts import render_to_response
 from django.template.loader import render_to_string
 from django.contrib.gis.db.models.manager import GeoManager
+from django.contrib.gis.db.models import fields
+
+GEOM_FIELDS = [
+    fields.GeometryField,
+    fields.PointField,
+    fields.LineStringField,
+    fields.PolygonField,
+    fields.MultiPointField,
+    fields.MultiLineStringField,
+    fields.MultiPolygonField,
+    fields.GeometryCollectionField,
+]
 
 def _get_name(self):
     return self._meta.db_table
@@ -10,16 +23,26 @@ def _get_name(self):
 def _get_model_name(self):
     return self._meta.object_name
 
-def index(request):
-    database_tables = []
+def _get_properties(self):
+    properties = {}
+    for field in self._meta.fields:
+        if type(field) not in GEOM_FIELDS:
+            properties[field.name] = getattr(self, field.name)
+    return json.dumps(properties)
+
+KOWLOON_MODELS = {}
+def monkey_patch_models():
     for model in [getattr(database_models, x) for x in dir(database_models)]:
         if type(model) == ModelBase and type(model.objects) == GeoManager:
             model.__str__ = _get_name
-            model.model_name = _get_model_name
-            database_tables.append(model)
+            model.get_model_name = _get_model_name
+            model.get_properties = _get_properties
+            KOWLOON_MODELS[str(model)] = model
+monkey_patch_models()
 
+def index(request):
     return render_to_response("kowloon/index.html", {
-            'database_tables': database_tables
+            'database_tables': sorted(KOWLOON_MODELS.values())
         })
 
 
