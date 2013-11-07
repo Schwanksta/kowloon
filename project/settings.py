105b0253758c475d985dd43448c7f9986d696f42
Ken Schwencke <schwank@gmail.com>
2013-11-07 10:37:22 -0800

    Upped Django to 1.6

--- project/settings.py
+++ project/settings.py
@@ -161,27 +161,27 @@ LOGGING = {
     }
 }
 
-# Django debug toolbar configuration
-if DEBUG_TOOLBAR:
-    # Add middleware
-    MIDDLEWARE_CLASSES += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
-    # JavaScript panels for the deveopment debugging toolbar
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-    )
-    # Debug toolbar app
-    INSTALLED_APPS += ('debug_toolbar',)
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-        'HIDE_DJANGO_SQL': False,
-    }
+## Django debug toolbar configuration
+#if DEBUG_TOOLBAR:
+#    # Add middleware
+#    MIDDLEWARE_CLASSES += (
+#        'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    )
+#    # JavaScript panels for the deveopment debugging toolbar
+#    DEBUG_TOOLBAR_PANELS = (
+#        'debug_toolbar.panels.version.VersionDebugPanel',
+#        'debug_toolbar.panels.timer.TimerDebugPanel',
+#        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+#        'debug_toolbar.panels.headers.HeaderDebugPanel',
+#        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+#        'debug_toolbar.panels.template.TemplateDebugPanel',
+#        'debug_toolbar.panels.sql.SQLDebugPanel',
+#        'debug_toolbar.panels.signals.SignalDebugPanel',
+#        'debug_toolbar.panels.logger.LoggingPanel',
+#    )
+#    # Debug toolbar app
+#    INSTALLED_APPS += ('debug_toolbar',)
+#    DEBUG_TOOLBAR_CONFIG = {
+#        'INTERCEPT_REDIRECTS': False,
+#        'HIDE_DJANGO_SQL': False,
+#    }
