b7a9cc8819c7cc5ca95d5df4dc8823ae7ad3353f
Ken Schwencke <schwank@gmail.com>
2014-03-04 16:35:31 -0500

    dox

--- kowloon/management/commands/inspectdb.py
+++ kowloon/management/commands/inspectdb.py
@@ -1,6 +1,6 @@
 ###
 # This is an overridden inspectdb, because Django enforces max_length on 
-# CharFields, and PostgreSQL does not. Because of this, if you load shapefile
+# CharFields and PostgreSQL does not. Because of this, if you load shapefile
 # data into PG it might not contain a max_length. When Django introspects
 # the database when you initially run the kowloon command, the lack of 
 # max_length will throw errors.
@@ -14,11 +14,17 @@ class Command(InspectDBCommand):
 
     def handle_inspection(self, options):
         table_name = options.get('table_name', None)
+        # If we supply a table name, we will send a filter function to the 
+        # base inspectdb command that filters to just that table.
         if table_name:
             options['table_name_filter'] = lambda x:  x == table_name
         return super(InspectDBCommand, self).handle_inspection(options)
 
     def get_field_type(self, connection, table_name, row):
+        """
+        Runs all of the normal get_field_type hoo-ha for GeoDjango, but adds a
+        max_length to CharFields that don't have one.
+        """
         field_type, field_params, field_notes = super(Command, self).get_field_type(connection, table_name, row)
         if field_type == 'CharField' and not field_params.get('max_length', None):
             field_params['max_length'] = 500
